8d45545caa8d8aa16b4d9ce67febbf4b22edf969
kplnhsyn <kplnhsyn.49@gmail.com>
2024-04-05 16:18:49 +0200

    feat:movieFunction added for fetch

--- helpers/movieFunctions.js
+++ helpers/movieFunctions.js
@@ -5,8 +5,8 @@ export const getMovies = async (type) => {
   const res = await fetch(URL);
   //? next.js ile fetch api çekilen verileri default olarak cache'ler. bu özelliği option objesi ile değiştirebiliriz
   // const res = await fetch(URL, { cache: "force-cache" }); default
-  // const res = await fetch(URL, { cache: "no-store" }); cache'leme
-  //   const res = await fetch(URL, { next: { revalidate: 10 } }); belirli aralıklarla veriyi tekrar çek tekrar
+  // const res = await fetch(URL, { cache: "no-store" }); cache'lememe
+    // const res = await fetch(URL, { next: { revalidate: 10 } }); belirli aralıklarla veriyi tekrar çek tekrar
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
