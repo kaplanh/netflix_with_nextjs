04b5ef8a64afab9949867ab16e398e0b6ff73d8b
kplnhsyn <kplnhsyn.49@gmail.com>
2025-02-04 19:59:24 +0100

    favicon added in layout.js

--- app/layout.js
+++ app/layout.js
@@ -9,20 +9,27 @@ const inter = Inter({ subsets: ["latin"] });
 
 //! metadata objesi ve generateMetadata fonksiyonu sadece Server Componentlerden export edilebilir.
 export const metadata = {
-  title: "Netflix",
-  description: "a movie platform",
+    title: "Netflix",
+    description: "a movie platform",
+    icons: {
+        icon: [
+            { url: "/images/logo.png" },
+            { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+            { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+        ],
+    },
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <AuthContextProvider>
-          <Navbar />
-          {children}
-          <ToastContainer />
-        </AuthContextProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <AuthContextProvider>
+                    <Navbar />
+                    {children}
+                    <ToastContainer />
+                </AuthContextProvider>
+            </body>
+        </html>
+    );
 }
