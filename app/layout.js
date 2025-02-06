c2f0ab83fac38c28e28fa69f3b20a495c108c0b0
kplnhsyn <kplnhsyn.49@gmail.com>
2025-02-06 01:06:48 +0100

    layout updated

--- app/layout.js
+++ app/layout.js
@@ -11,13 +11,13 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
     title: "Netflix",
     description: "a movie platform",
-    icons: {
-        icon: [
-            { url: "/images/logo.png" },
-            { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-            { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-        ],
-    },
+    // icons: {
+    //     icon: [
+    //         { url: "/images/logo.png" },
+    //         { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    //         { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    //     ],
+    // },
 };
 
 export default function RootLayout({ children }) {
